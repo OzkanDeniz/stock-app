020da2670b89c39181cf77e72694203886d663f1
ozkan <ozkandnz09@gmail.com>
2024-10-03 16:30:21 +0300

    register

--- src/services/useApiRequests.js
+++ src/services/useApiRequests.js
@@ -12,19 +12,19 @@ const useApiRequests = () => {
   const login = async (userData) => {
     // const BASE_URL = "https://14111.fullstack.clarusway.com";
 
-    dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
       const { data } = await axios.post(
         `${process.env.REACT_APP_BASE_URL}/auth/login`,
         userData
       );
       toastSuccessNotify("Login işlemi başarılı");
-      dispatch(loginSuccess(data))
+      dispatch(loginSuccess(data));
       navigate("stock"); //? useNavigate hookunu ya bir react component içinde çağırılabilir yada bir cosutom(özel) hooke da çağırılabilir burası bir js fonksiyonu yazdık bir component değil burada amcımız generic bir js fonsksiyonu yazmamızın amacı her yerde kullanabilmek için bu yüzden custom hook yazmalıyız (custom hook jsx return etmez ama içinde hook kullanılabilir bu yüzden ismini değiştirdik useApiRequests yaptık)
       console.log(data);
     } catch (error) {
       toastErrorNotify("Login işlemi başarısız");
-      dispatch(fetchFail())
+      dispatch(fetchFail());
       console.log(error);
     }
   };
