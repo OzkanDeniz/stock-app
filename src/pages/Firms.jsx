467824e6e2b5bc59a96d5c872d9a458ecddff6cd
ozkan <ozkandnz09@gmail.com>
2024-10-05 15:01:32 +0300

    getFirms

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
+
 
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const getFirm = async () => {
+    try {
+      const { data } = await axios(`${process.env.REACT_APP_BASE_URL}/firms`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //?Sayfa yüklendikten sonra firmaları getir.
+  useEffect(() => {
+    getFirm();
+  }, []);
+
   return <div>Firms</div>;
 };
 
