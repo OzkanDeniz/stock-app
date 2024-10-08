8486e81015d74d01803cddff4e8096fcdb83a105
ozkan <ozkandnz09@gmail.com>
2024-10-08 23:47:49 +0300

    redux-persist error fix

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,7 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
 
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 const persistConfig = {
@@ -14,12 +23,18 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: persistReducer, //Login olmuşmu olmamışmı,token saklanmışmı saklanmamışmı
+    auth: persistedReducer, //Login olmuşmu olmamışmı,token saklanmışmı saklanmamışmı
     stock: stockReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
 export default store;
