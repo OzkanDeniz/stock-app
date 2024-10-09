4d07900b5c87c6ce2970fca4783c749647819f15
ozkan <ozkandnz09@gmail.com>
2024-10-09 18:14:17 +0300

    input state

--- src/components/FirmModal.jsx
+++ src/components/FirmModal.jsx
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 const style = {
   position: "absolute",
@@ -21,7 +22,16 @@ export default function FirmModal({ open, handleClose }) {
   //   const [open, setOpen] = React.useState(false);
   //   const handleOpen = () => setOpen(true);
   //   const handleClose = () => setOpen(false);
-
+  const [data,setData] = useState({
+    image: "",
+    address: "",
+    phone: "",
+    name: "",
+  });
+  const handleChange = (e)=>{
+    setData({...data, [e.target.name]:e.target.value})
+  }
+  console.log(data)
   return (
     <div>
       <Modal
@@ -33,12 +43,44 @@ export default function FirmModal({ open, handleClose }) {
         <Box sx={style}>
           <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
             <TextField
-              label="Email"
-              name="email"
-              id="email"
-              type="email"
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
               variant="outlined"
+              value={data.name}
+              onChange={handleChange}
             />
+            <TextField
+              label="Phone"
+              name="phone"
+              id="phone"
+              type="tel"
+              variant="outlined"
+              value={data.phone}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Address"
+              name="address"
+              id="address"
+              type="text"
+              variant="outlined"
+              value={data.address}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Image"
+              name="image"
+              id="image"
+              type="url"
+              variant="outlined"
+              value={data.image}
+              onChange={handleChange}
+            />
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
           </Box>
         </Box>
       </Modal>
