210850a84e6694e6e6cf9d37c7c6f517243a34eb
ozkan <ozkandnz09@gmail.com>
2024-10-09 15:51:28 +0300

    FirmModal

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import { useSelector } from "react-redux";
 import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/FirmModal";
 
 // import { useSelector } from "react-redux";
 // import axios from "axios";
@@ -26,7 +27,6 @@ const Firms = () => {
 
   const { getStock } = useStockRequests();
   const { firms } = useSelector((state) => state.stock);
-  
 
   //?Sayfa yüklendikten sonra firmaları getir.
   useEffect(() => {
@@ -40,12 +40,15 @@ const Firms = () => {
       <Typography variant="h5" color={"error"} mb={2}>
         Firms
       </Typography>
-      <Button variant="contained" sx={{mb:2}}>NEW FİRM</Button>
+      <Button variant="contained" sx={{ mb: 2 }}>
+        NEW FİRM
+      </Button>
+      <FirmModal/>
 
       <Grid container justifyContent={"center"} gap={2}>
-        {firms.map((firm) => (
-          <Grid item key={firm._id}>
-            <FirmCard firm={firm}/>
+        {firms.map((firm, index) => (
+          <Grid item key={index}>
+            <FirmCard firm={firm} />
           </Grid>
         ))}
       </Grid>
