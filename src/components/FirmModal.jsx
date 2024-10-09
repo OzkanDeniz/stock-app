40e3725a559f936de5fa9d26c7615d4509d782a4
ozkan <ozkandnz09@gmail.com>
2024-10-09 17:44:11 +0300

    textfield

--- src/components/FirmModal.jsx
+++ src/components/FirmModal.jsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
 
 const style = {
   position: "absolute",
@@ -16,7 +17,7 @@ const style = {
   p: 4,
 };
 
-export default function FirmModal({open, handleClose}) {
+export default function FirmModal({ open, handleClose }) {
   //   const [open, setOpen] = React.useState(false);
   //   const handleOpen = () => setOpen(true);
   //   const handleClose = () => setOpen(false);
@@ -30,12 +31,15 @@ export default function FirmModal({open, handleClose}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            <TextField
+              label="Email"
+              name="email"
+              id="email"
+              type="email"
+              variant="outlined"
+            />
+          </Box>
         </Box>
       </Modal>
     </div>
